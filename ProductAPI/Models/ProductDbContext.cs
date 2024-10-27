e6bee30929a7b11a6e9534b5f592f10208ac8b81
zaba <bilonizkavik@gmail.com>
2024-10-27 15:17:42 +0100

    Implemented ProductDBContext.cs based on Library/Product with Entity Framework.
    Implemented necessary services in Program.cs and  also the connection string in appsettings,json

---
+++ ProductAPI/Models/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Data;
+using System.Text;
+
+namespace ProductAPI.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Employees { get; set; }
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
