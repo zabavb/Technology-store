62656e4553007adee9bb177d8f9932237b30b174
zaba <bilonizkavik@gmail.com>
2024-10-27 08:57:25 +0100

    Add project files.

---
+++ ProductClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
