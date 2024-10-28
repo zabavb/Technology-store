5725b039048d27104b77dc34e82b6ff35b069434
zaba <bilonizkavik@gmail.com>
2024-10-28 13:47:00 +0100

    Implemented password hashing by toute api/Users/HashPassword.

--- UserAPI/Controllers/UsersController.cs
+++ UserAPI/Controllers/UsersController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Library.Models;
 using UserAPI.Models;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace UserAPI.Controllers
 {
@@ -44,6 +46,20 @@ namespace UserAPI.Controllers
                 return NotFound();
 
             return user;
+        }
+
+        // POST: api/Users/HashPassword
+        [HttpPost("HashPassword")]
+        public async Task<IActionResult> HashPassword([FromBody] string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+                string hashedPassword = Convert.ToBase64String(hashBytes);
+
+                return Ok(hashedPassword);
+            }
         }
 
         // POST: api/Users
