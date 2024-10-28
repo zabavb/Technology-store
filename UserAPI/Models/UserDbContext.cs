d046b69ca7d309b12615fb25ed5d0aa77e41e238
zaba <bilonizkavik@gmail.com>
2024-10-28 14:12:41 +0100

    Implemented into UserAPI/UserDbContext - "onModelCreating()" with admin, moderator and user.

--- UserAPI/Models/UserDbContext.cs
+++ UserAPI/Models/UserDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using Library.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Data;
+using System.Text;
 
 namespace UserAPI.Models
 {
@@ -10,6 +13,41 @@ namespace UserAPI.Models
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
         {
             Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            string pass1 = "1234", pass2 = "4321", pass3 = "1234";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7074/");
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(pass1), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PostAsync("gateway/users/hashpassword", content).Result;
+                    if (response.IsSuccessStatusCode)
+                        pass1 = response.Content.ReadAsStringAsync().Result;
+                }
+
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(pass2), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PostAsync("gateway/users/hashpassword", content).Result;
+                    if (response.IsSuccessStatusCode)
+                        pass2 = response.Content.ReadAsStringAsync().Result;
+                }
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(pass3), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PostAsync("gateway/users/hashpassword", content).Result;
+                    if (response.IsSuccessStatusCode)
+                        pass3 = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            modelBuilder.Entity<User>().HasData(
+                new User(1, "zabavb",  "Viktor", "Bilonizhka", 18, "bilonizkavik@gmail.com", "+380000000000", pass1, RoleType.Admin.ToString()),
+                new User(2, "olegoovich", "Oleg", "Olegovich", 23, "olegovichmail@gmail.com", "+440000000000", pass2, RoleType.Moderator.ToString()),
+                new User(3, "optimus_prime", "Optimus", "Prime", 45, "optimusprimemail@gmail.com", "+910000000000", pass3, RoleType.User.ToString())
+            );
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
