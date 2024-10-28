74a929c557fc439dcf0e8d70fb8699f4a31bc7a3
zaba <bilonizkavik@gmail.com>
2024-10-28 12:38:12 +0100

    Implemented Library reference, Models/UserDbContext, connection string in appsettings.json

---
+++ UserAPI/Models/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserAPI.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
