2f47e25c80fbf9a86ced17b7519f28f683223330
zaba <bilonizkavik@gmail.com>
2024-10-31 18:45:55 +0100

    Removed ASP.NET MVC template.

--- Client/Program.cs
+++ Client/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Employee}/{action=Login}/{id?}");
+    pattern: "{controller=Users}/{action=Login}/{id?}");
 
 app.Run();
