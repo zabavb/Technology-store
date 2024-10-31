2ce09a9d28e0ed7858007b47f764a645ef269cf4
zaba <bilonizkavik@gmail.com>
2024-10-31 15:46:37 +0100

    Implemeneted ManageProductViewModel.cs in Products folder.

---
+++ Client/Models/Products/ManageProductViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.Products
+{
+    public class ManageProductViewModel
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Brand is required")]
+        [StringLength(16, MinimumLength = 2, ErrorMessage = "Brand must be in range between 2 and 16 characters")]
+        public string Brand { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Model is required")]
+        [StringLength(16, MinimumLength = 2, ErrorMessage = "Model must be in range between 2 and 16 characters")]
+        public string Model { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Producer is required")]
+        [StringLength(16, MinimumLength = 2, ErrorMessage = "Producer must be in range between 2 and 16 characters")]
+        public string Producer { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0, 2000000, ErrorMessage = "Price must be in range between 0 and 2.000.000")]
+        public double Price { get; set; }
+
+        [StringLength(512, MinimumLength = 16, ErrorMessage = "Producer must be in range between 16 and 512 characters")]
+        public string? Details { get; set; } = string.Empty;
+
+        public ManageProductViewModel(long id, string brand, string model, string producer, double price, string? details)
+        {
+            Id = id;
+            Brand = brand;
+            Model = model;
+            Producer = producer;
+            Price = price;
+            Details = details;
+        }
+
+        public ManageProductViewModel() { }
+    }
+}
