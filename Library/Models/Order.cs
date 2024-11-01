ec3f49391d3aacfa18ccf554b8495ab64d118e15
zaba <bilonizkavik@gmail.com>
2024-11-01 15:17:37 +0100

    Implemented Order.cs

---
+++ Library/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public User Receiver { get; set; } = new();
+        public Product Product { get; set; } = new();
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Payment { get; set; } = PaymentType.None.ToString();
+    }
+}
