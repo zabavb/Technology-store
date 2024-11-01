f51acb0fb122977a6e19aad462aac69fe4af8c56
zaba <bilonizkavik@gmail.com>
2024-11-01 15:48:55 +0100

    Implemented ProductsController.cs / ManageController.cs

---
+++ Client/Controllers/ManageController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class ManageController : Controller
+    {
+        
+    }
+}
---
+++ Client/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Client.Models;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Client.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+    }
+}
