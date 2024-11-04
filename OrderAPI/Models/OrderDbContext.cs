490d4ec709339ce344077a695d30ed9a434ad348
Viktor <bilonizkavik@gmail.com>
2024-11-04 15:30:17 +0100

    Implemented Project OrderAPI.

---
+++ OrderAPI/Models/OrderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace OrderAPI.Models
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .Property(e => e.DeliveryDate)
+                .HasConversion(
+                    d => d.ToDateTime(TimeOnly.MinValue),
+                    d => DateOnly.FromDateTime(d)
+                );
+        }
+
+    }
+}
